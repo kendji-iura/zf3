fbaa2251cfe6274e82558666709ef8b34d02807b
Kendji Iura <kendji.iura@essentia.com.br>
2024-05-06 19:58:43 +0000

    curso finalizado

--- module/Blog/src/Model/PostTable.php
+++ module/Blog/src/Model/PostTable.php
@@ -1,12 +1,13 @@
 <?php
 
+
 namespace Blog\Model;
 
+use Zend\Db\TableGateway\Exception\RuntimeException;
 use Zend\Db\TableGateway\TableGatewayInterface;
 
 class PostTable
 {
-    
     private $tableGateway;
 
     public function __construct(TableGatewayInterface $tableGateway)
@@ -22,14 +23,44 @@ class PostTable
     public function save(Post $post)
     {
         $data = [
-            "title"=> $post->title,
-            "content"=> $post->content,
+            'title' => $post->title,
+            'content' => $post->content
         ];
 
-        if ((int) $post->id === 0) {
+        $id = (int)$post->id;
+
+        if ($id === 0) {
             $this->tableGateway->insert($data);
             return;
         }
+
+        if (!$this->find($id)) {
+            throw new RuntimeException(sprintf(
+                'Could not retrieve the row %d', $id
+            ));
+        }
+
+        $this->tableGateway->update($data, ['id' => $id]);
+    }
+
+    public function find($id)
+    {
+        $id = (int)$id;
+        $rowset = $this->tableGateway->select(['id' => $id]);
+        $row = $rowset->current();
+
+        if (!$row) {
+            throw new RuntimeException(sprintf(
+                'Could not retrieve the row %d', $id
+            ));
+        }
+
+        return $row;
+    }
+
+    public function delete($id)
+    {
+        $this->tableGateway->delete(['id' => (int)$id]);
     }
 
 }
